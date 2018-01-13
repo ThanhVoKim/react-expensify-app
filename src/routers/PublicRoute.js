564f31b385110a02cfbbe0f2ca97a30460e41383
Thanh Vo Kim <13829722+ThanhVoKim@users.noreply.github.com>
2018-01-13 16:04:13 +0700

    Update PublicRoute.js

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -2,29 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
-// export const PublicRoute = ({
-//   isAuthenticated,
-//   component: Component,
-//   ...rest
-// }) => (
-//   <Route
-//     {...rest}
-//     component={(props) => {
-//       if (isAuthenticated) {
-//         return <Redirect to="/dashboard" />;
-//       }
-
-//       return <Component {...props} />;
-//     }}
-//   />
-// );
-
-// const mapStateToProps = (state) => ({
-//   isAuthenticated: !!state.auth.uid
-// });
-
-// export default connect(mapStateToProps)(PublicRoute);
-
 export default (WrapComponent) => {
   const Authenticate = (props) => {
     if (props.isAuthenticated) {
